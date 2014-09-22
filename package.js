c72aea5c933350b05db51964309e38f1da6b9dda
Matt Brennan <mattyb1000@gmail.com>
2014-09-22 22:36:53 +0100

    fix

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "A javascript library for multi-touch gestures",
-  version: "2.0.3",
+  version: "2.0.3.1",
   git: "https://github.com/quarterto/meteor-hammer.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.2');
-  api.addFiles('hammer/hammer.js');
+  api.addFiles('hammer/hammer.js', 'client');
 });
 
