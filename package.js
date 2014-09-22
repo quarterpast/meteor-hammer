9e9224e6e0c07a9dbf9883d2228a6253acf7f435
Matt Brennan <mattyb1000@gmail.com>
2014-09-22 22:32:25 +0100

    init

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: "A javascript library for multi-touch gestures",
+  version: "2.0.3",
+  git: "https://github.com/quarterto/meteor-hammer.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.2.2');
+  api.addFiles('hammer/hammer.js');
+});
+
